c0247f8300aad7fdffcca85d7aadf8c1f00e0a8c
Eran Cohen <er11cohen@gmail.com>
2022-06-09 15:11:04 +0300

    Remove redundant code of sdk version

--- hokLeisrael/src/main/java/com/eran/hokleisrael/AppendixActivity.java
+++ hokLeisrael/src/main/java/com/eran/hokleisrael/AppendixActivity.java
@@ -1,7 +1,6 @@
 package com.eran.hokleisrael;
 
 
-import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
@@ -14,25 +13,14 @@ public class AppendixActivity extends Activity {
 
     Parash parash;
 
-    @SuppressLint("NewApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appendix);
-
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.HONEYCOMB) {
             ActionBar actionBar = getActionBar();
             actionBar.setDisplayHomeAsUpEnabled(true);
-        }
         parash = new Parash();
     }
-
-	/*@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.appendix, menu);
-		return true;
-	}*/
 
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -47,26 +35,16 @@ public class AppendixActivity extends Activity {
         int day = -1;//no day
 
         Intent intent = new Intent(getApplicationContext(), WebActivity.class);
-//	    	intent.putExtra("day", day);
-//	    	intent.putExtra("humashEn", "appendix");
-//	    	intent.putExtra("parshEn", "Intro");
-//	    	intent.putExtra("parshHe", "הקדמות");
         parash.setDay(day);
         parash.setHumashEn("appendix");
         parash.setParshEn("Intro");
         parash.setParshHe("הקדמות");
         intent.putExtra("parash", parash);
         startActivity(intent);
-
     }
 
     public void selectMamadot(View v) {
         Intent intent = new Intent(getApplicationContext(), DayActivity.class);
-//	    	intent.putExtra("humashEn", "appendix");
-//	    	intent.putExtra("parshEn", "sederMamadot");
-//	    	intent.putExtra("parshHe", "מעמדות");
-//	    	intent.putExtra("weekly", true);
-
         parash.setHumashEn("appendix");
         parash.setParshEn("sederMamadot");
         parash.setParshHe("מעמדות");
@@ -78,11 +56,6 @@ public class AppendixActivity extends Activity {
 
     public void selectOrhotHaim(View v) {
         Intent intent = new Intent(getApplicationContext(), DayActivity.class);
-//	    	intent.putExtra("humashEn", "appendix");
-//	    	intent.putExtra("parshEn", "orhotHaim");
-//	    	intent.putExtra("parshHe", "אורחות חיים");
-//	    	intent.putExtra("weekly", true);
-
         parash.setHumashEn("appendix");
         parash.setParshEn("orhotHaim");
         parash.setParshHe("אורחות חיים");
@@ -95,11 +68,6 @@ public class AppendixActivity extends Activity {
     public void selectHatzatilHakatan(View v) {
         int day = -1;//no day
         Intent intent = new Intent(getApplicationContext(), WebActivity.class);
-//	    	intent.putExtra("day", day);
-//	    	intent.putExtra("humashEn", "appendix");
-//	    	intent.putExtra("parshEn", "hatzatilHakatan");
-//	    	intent.putExtra("parshHe", "הצעטיל הקטן");
-
         parash.setDay(day);
         parash.setHumashEn("appendix");
         parash.setParshEn("hatzatilHakatan");
